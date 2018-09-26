c95f95c8ed97e1c3fa90b8bda1cd89dd1bd6b03a
gescobar-technisys <gescobar@technisys.com>
2018-09-26 17:50:51 -0300

    Agrego gastos

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import '../css/App.css';
 import Header from './Header';
 import Formulario from './Formulario';
+import Listado from './Listado';
 
 class App extends Component {
+
+  state = {
+    presupuesto: '',
+    restante: '',
+    gastos: {}
+  }
+
+  agregarGasto = gasto => {
+    const gastos = {...this.state.gastos};
+
+    gastos[`gasto${Date.now()}`] = gasto;
+
+    this.setState({
+      gastos
+    })
+  }
+
   render() {
     return (
       <div className = "App container">
@@ -14,10 +32,14 @@ class App extends Component {
       <div className = "contenido-principal contenido">
         <div className = "row">
           <div className = "one-half column">
-              <Formulario />
+              <Formulario 
+                agregarGasto = {this.agregarGasto}
+              />
           </div>
           <div className = "one-half column">
-            
+              <Listado 
+                gastos={this.state.gastos}
+              />
           </div>
         </div>
       </div>
--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 
 class FormularioGasto extends Component {
+    
+    cantidadGasto = React.createRef();
+    nombreGasto = React.createRef();
+    
+    crearGasto = (e) => {
+        e.preventDefault();
+        const gasto = {
+            nombreGasto : this.nombreGasto.current.value,
+            cantidadGasto : this.cantidadGasto.current.value
+        }
+
+        this.props.agregarGasto(gasto);
+        e.currentTarget.reset();
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit = {this.crearGasto}>
                 <h2>Agrega tus gastos aqui</h2>
                 <div className="campo">
                     <label>Nombre Gasto</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. Transporte" />
+                    <input ref = {this.nombreGasto} className="u-full-width" type="text" placeholder="Ej. Transporte" />
                 </div>
 
                 <div className="campo">
                     <label>Cantidad</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. 300" />
+                    <input ref = {this.cantidadGasto} className="u-full-width" type="text" placeholder="Ej. 300" />
                 </div>
 
                 <input className="button-primary u-full-width" type="submit" value="Agregar" />
