c2fc3564b3e6ad16a076b7839ec1b5df733f8452
gonzaloescobar <gonzalo.david.escobar@gmail.com>
2018-09-26 10:28:12 -0300

    Add Header component

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hola</h1>
+      <div className = "App container">
+      
+      <Header
+        titulo = 'Gasto Semanal'
+      />
+
+      </div>
     );
   }
 }
